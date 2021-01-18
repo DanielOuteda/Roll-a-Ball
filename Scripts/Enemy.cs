048ad3bb27830d696936d8efe1f937f66fdfbf82
Daniel <56076524+DanielOuteda@users.noreply.github.com>
2021-01-18 12:31:20 +0100

    Juego acabado y añadido Enemigo completo

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent pathfinder;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pathfinder.SetDestination(target.position);
+        Debug.Log(target.position);
+    }
+}
--- Scripts/PlayerControler.cs
+++ Scripts/PlayerControler.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerControler : MonoBehaviour
 {
     public float speed = 0;
-
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     
@@ -15,6 +18,10 @@ public class PlayerControler : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void  OnMove(InputValue movementValue)
@@ -25,13 +32,39 @@ public class PlayerControler : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 5)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
 
+            SetCountText();
+        }
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            if (count>0)
+            {
+            count = count - 1;
+            SetCountText();
+            }
+            other.gameObject.transform.position=new Vector4(0,1,0);
+        }
+    }
 }
